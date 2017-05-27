ec15298626e0db12f8684cda3f7b54332017d500
utkarshcmu <utkarsh.cmu@gmail.com>
2017-05-27 03:26:24 -0700

    upload endpoint to upload a file to server

--- src/app.rb
+++ src/app.rb
@@ -2,6 +2,18 @@
 
 require 'sinatra'
 
-get '/process' do
-	'Upload csv file!'
-end 
+get "/" do
+  erb :form
+end
+
+post '/save_image' do
+  
+  @filename = params[:file][:filename]
+  file = params[:file][:tempfile]
+
+  File.open("./public/#{@filename}", 'wb') do |f|
+    f.write(file.read)
+  end
+  
+  erb :show_image
+end
